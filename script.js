1d3a51d2d7457f82768fa106fa5e14b8bcfe445d
Maulana Wahid A <mau.wahid@gmail.com>
2024-06-27 22:37:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function convertJson() {
+function convertJsonToVault() {
     const input = document.getElementById('inputJson').value;
     let json;
     try {
@@ -34,4 +34,44 @@ function convertJson() {
     result[''] = ''; // Menambahkan atribut terakhir "": ""
 
     document.getElementById('outputJson').value = JSON.stringify(result, null, 2);
+    showToast();
+}
+
+function convertVaultToJson() {
+    const input = document.getElementById('outputJson').value; // Mengambil input dari textarea sebelah kanan
+    let vaultJson;
+    try {
+        vaultJson = JSON.parse(input);
+    } catch (e) {
+        alert('Invalid JSON Vault');
+        return;
+    }
+
+    const result = {};
+    Object.keys(vaultJson).sort().forEach(key => {
+        if (key === '') return; // Abaikan atribut terakhir "": ""
+        const keys = key.split('__');
+        keys.reduce((acc, cur, idx) => {
+            if (idx === keys.length - 1) {
+                let value = vaultJson[key];
+                if (value === '"true"') value = true;
+                else if (value === '"false"') value = false;
+                else if (!isNaN(value) && value.trim() !== '') value = parseFloat(value);
+                else value = value.replace(/^"|"$/g, '');
+                acc[cur] = value;
+            } else {
+                acc[cur] = acc[cur] || {};
+            }
+            return acc[cur];
+        }, result);
+    });
+
+    document.getElementById('inputJson').value = JSON.stringify(result, null, 2); // Menempatkan hasil di textarea sebelah kiri
+    showToast();
+}
+
+function showToast() {
+    const toast = document.getElementById('toast');
+    toast.className = 'toast show';
+    setTimeout(() => { toast.className = toast.className.replace('show', ''); }, 3000);
 }
